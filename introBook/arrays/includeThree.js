e89b7093b97887020c319ac8f798c375cd7e703f
Scott Hoecker <hoeckers@gmail.com>
2021-06-24 13:39:45 +0000

    finished array exercises, trying out a new branch and stuff

---
+++ introBook/arrays/includeThree.js
@@ -0,0 +1,7 @@
+let numbers1 = [1, 3, 5, 7, 9, 11];
+let numbers2 = [];
+let numbers3 = [2, 4, 6, 8];
+
+let arrays = [numbers1, numbers2, numbers3];
+
+arrays.forEach(array => console.log(array.includes(3)));
